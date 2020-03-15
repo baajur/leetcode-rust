22d64d23ff604f1c2ffaf5e8e35bd6dd3521d2e7
Yusuke Tanaka <wing0920@gmail.com>
2020-03-15 18:40:03 +0900

    Fix function name

--- src/bin/283.rs
+++ src/bin/283.rs
@@ -1,7 +1,7 @@
 struct Solution;
 
 impl Solution {
-    pub fn move_zeros(nums: &mut Vec<i32>) {
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
         let len = nums.len();
         let mut last_non_zero_at = 0;
         for i in 0..len {
@@ -18,7 +18,7 @@ impl Solution {
 
 fn main() {
     let mut v = vec![0, 1, 0, 3, 12];
-    Solution::move_zeros(&mut v);
+    Solution::move_zeroes(&mut v);
     dbg!(&v);
 }
 
@@ -29,27 +29,27 @@ mod tests {
     #[test]
     fn test_move_zeros() {
         let mut v = vec![0, 1, 0, 3, 12];
-        Solution::move_zeros(&mut v);
+        Solution::move_zeroes(&mut v);
         assert_eq!(v, vec![1, 3, 12, 0, 0]);
 
         let mut v = vec![0, 0, 0];
-        Solution::move_zeros(&mut v);
+        Solution::move_zeroes(&mut v);
         assert_eq!(v, vec![0, 0, 0]);
 
         let mut v = vec![];
-        Solution::move_zeros(&mut v);
+        Solution::move_zeroes(&mut v);
         assert_eq!(v, vec![]);
 
         let mut v = vec![1];
-        Solution::move_zeros(&mut v);
+        Solution::move_zeroes(&mut v);
         assert_eq!(v, vec![1]);
 
         let mut v = vec![0, 0, 0, 0, 1];
-        Solution::move_zeros(&mut v);
+        Solution::move_zeroes(&mut v);
         assert_eq!(v, vec![1, 0, 0, 0, 0]);
 
         let mut v = vec![1, 2, 3, 4, 5];
-        Solution::move_zeros(&mut v);
+        Solution::move_zeroes(&mut v);
         assert_eq!(v, vec![1, 2, 3, 4, 5]);
     }
 }
