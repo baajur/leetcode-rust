f8febbadeb409b0858c7ca746a7d67dc627b6559
Yusuke Tanaka <wing0920@gmail.com>
2020-03-19 00:42:38 +0900

    146

---
+++ src/bin/146.rs
@@ -0,0 +1,19 @@
+struct LRUCache {}
+
+impl LRUCache {
+    fn new(capacity: i32) -> Self {
+        todo!();
+    }
+
+    fn get(&self, key: i32) -> i32 {
+        42
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        ()
+    }
+}
+
+fn main() {
+    ()
+}
